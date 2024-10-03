7db37315db08e9e305022e05f3e926e8125e4fca
chi-g <chijindu.nwokeohuru@gmail.com>
2024-10-03 05:11:40 +0100

    new commits

--- src/App.js
+++ src/App.js
@@ -1,23 +1,32 @@
 import React from 'react';
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';  // Updated imports
-import { AuthProvider } from './auth/AuthContext';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import { AuthProvider, useAuth } from './auth/AuthContext';
 import SignIn from './auth/SignIn';
+import SignUp from './auth/SignUp';
 import Decks from './components/Decks';
 import Navbar from './components/Navbar';
+import Welcome from './components/Welcome';
 
 function App() {
     return (
         <AuthProvider>
             <Router>
                 <Navbar />
-                <Routes>  {/* Replace Switch with Routes */}
-                    {/* Use element prop instead of component */}
-                    <Route path="/" element={<SignIn />} />  
-                    <Route path="/decks" element={<Decks />} />
+                <Routes>
+                    <Route path="/" element={<Welcome />} />
+                    <Route path="/signin" element={<SignIn />} />
+                    <Route path="/signup" element={<SignUp />} />
+                    <Route path="/decks" element={<ProtectedRoute component={Decks} />} />
                 </Routes>
             </Router>
         </AuthProvider>
     );
+}
+
+function ProtectedRoute({ component: Component }) {
+    const { currentUser } = useAuth();
+    
+    return currentUser ? <Component /> : <SignIn />;
 }
 
 export default App;
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,12 +1,14 @@
-
 import React from 'react';
 import { Link } from 'react-router-dom';
 
 const Navbar = () => {
     return (
         <nav>
-            <Link to="/">Home</Link>
-            <Link to="/decks">Decks</Link>
+            <ul>
+                <li><Link to="/">Sign In</Link></li>
+                <li><Link to="/signup">Sign Up</Link></li>
+                <li><Link to="/decks">Decks</Link></li>
+            </ul>
         </nav>
     );
 };
